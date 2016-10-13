eecdce9905108833602dec5beda26a42e342e20b
Sardar Molaei <molaei.s@gmail.com>
2016-10-13 21:41:36 +0000

    made major adjustments

--- browse-reddit.js
+++ browse-reddit.js
@@ -1,6 +1,7 @@
 var inquirer = require('inquirer');
 var redditFunctions = require('./reddit');
 require('colors');
+require("longjohn");
 
 function displayPost(post) {
     console.log(post.data.title.bold);
@@ -16,72 +17,104 @@ function displayListSubreddit(name) {
     console.log('\n');
 }
 
-    var menuChoices = [{
-        name: 'Show homepage',
-        value: 'HOMEPAGE'
-    }, {
-        name: 'Show subreddit',
-        value: 'SUBREDDIT'
-    }, {
-        name: 'List subreddits',
-        value: 'SUBREDDITS'
-    }];
+var menuChoices = [{
+    name: 'Show homepage',
+    value: 'HOMEPAGE'
+}, {
+    name: 'Show subreddit',
+    value: 'SUBREDDIT'
+}, {
+    name: 'List subreddits',
+    value: 'SUBREDDITS'
+}];
 
-    function mainMenu() {
-        inquirer.prompt({
-            type: 'list',
-            name: 'menu',
-            message: 'What do you want to do?',
-            choices: menuChoices
-        }).then(
-            function(answers) {
-                if (answers.menu === 'HOMEPAGE') {
-                    redditFunctions.getHomepage(function(err, homepagePosts) {
-                        if (err) {
-                            console.log('There was a problem, try again later');
-                        }
-                        else {
-                            homepagePosts.forEach(displayPost);
-                            mainMenu();
-                        }
-                    });
-                }
-                else if (answers.menu === 'SUBREDDIT') {
-                    inquirer.prompt({
-                        type: 'input',
-                        name: 'Subreddit Name',
-                        message: 'Write the name of the subreddit you wish to visit'
-
-                    }).then(
-                        function(input) {
-                            redditFunctions.getSubreddit(input['Subreddit Name'], function(err, subredditTitles) {
-                                if (err) {
-                                    console.log("There was a problem loading subreddits");
-                                }
-                                else {
-                                    subredditTitles.forEach(displayPost);
-                                    mainMenu();
-                                }
-
-                            });
-                        }
-                    );
-                }
-                else if (answers.menu === 'SUBREDDITS') {
-                    redditFunctions.getSubreddits(function(err, listofSubr) {
-                        if (err) {
-                            console.log("There was a problem loading subreddits");
-                        }
-                        else {
-                            listofSubr.forEach(displayListSubreddit);
-                            mainMenu();
-                        }
-                    });
-                }
-
-
+function mainMenu() {
+    inquirer.prompt({
+        type: 'list',
+        name: 'menu',
+        message: 'What do you want to do?',
+        choices: menuChoices
+    }).then(
+        function(answers) {
+            if (answers.menu === 'HOMEPAGE') {
+                redditFunctions.getHomepage(function(err, homepagePosts) {
+                    if (err) {
+                        console.log('There was a problem, try again later');
+                    }
+                    else {
+                        homepagePosts.forEach(displayPost);
+                        mainMenu();
+                    }
+                });
             }
-        );
-    }
+            else if (answers.menu === 'SUBREDDIT') {
+                
+                inquirer.prompt({
+                    type: 'input',
+                    name: 'Subreddit Name',
+                    message: 'Write the name of the subreddit you wish to visit'
+
+                }).then(
+                    function(input) {
+                        redditFunctions.getSubreddit(input['Subreddit Name'], function(err, subredditTitles) {
+                            if (err) {
+                                console.log("There was a problem loading subreddits");
+                            }
+                            else {
+                                subredditTitles.forEach(displayPost);
+                                mainMenu();
+                            }
+
+                        });
+                    }
+                );
+            }
+            else if (answers.menu === 'SUBREDDITS') {
+                redditFunctions.getSubreddits(function(err, listofSubr) {
+                    if (err) {
+                        console.log("There was a problem loading subreddits");
+                    }
+                    else {
+                        var mapedArraySub = listofSubr.map(function(element) {
+                            return {
+                                name: element.data.title.bold,
+                                value: element.data.display_name
+                            }
+
+                        })
+                        inquirer.prompt({
+                            
+                            type: 'list',
+                            name: 'menu',
+                            message: 'Select the subreddit you wish to visit',
+                            choices: mapedArraySub
+                        
+                            
+                        }).then(
+                            
+                            function(input) {
+                                var userInput = input['menu'];
+                                redditFunctions.getSubreddit(userInput, function(err, res) {
+                                    if (err) {
+                                        console.log(err.stack);
+                                        console.log("There was a problem loading subreddit");
+                                    }
+                                    else {
+                                        res.forEach(displayPost);
+                                        mainMenu();
+                                    }
+                                })
+                            }
+                        )
+                        .catch(function(err) {
+                            console.log(err.stack);
+                        })
+                    }
+                });
+            }
+
+        }
+    );
+}
 
 mainMenu();
